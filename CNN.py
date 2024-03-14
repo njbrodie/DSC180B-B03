eecf2274d973899b3f4346f52cd8f42c8b628098
Benjamin Xue <bxtrey5@Benjamins-MacBook-Pro.local>
2024-03-14 14:08:22 -0700

    add CNN script

---
+++ CNN.py
@@ -0,0 +1,130 @@
+import numpy as np
+import xarray as xr
+import pandas as pd
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+import os
+from eofs.xarray import Eof
+import sys
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Activation, Conv2D, Flatten, Input, Reshape, AveragePooling2D, MaxPooling2D, Conv2DTranspose, TimeDistributed, LSTM, GlobalAveragePooling2D, BatchNormalization
+from tensorflow.keras.regularizers import l2
+import seaborn as sns
+
+import random
+
+from CNN_helper import get_nrmse_spatial, get_nrmse_global, get_nrmse, get_vpd, input_for_training, output_for_training, create_predictdand_data, normalize
+
+def get_rmse(test_scenario, preds):
+    hursTest = xr.open_dataset('vpd_data/hurs_' + test_scenario + '.nc')
+    tasTest = xr.open_dataset('vpd_data/tas_' + test_scenario + '.nc')
+    vpdTest = get_vpd(hursTest.hurs, tasTest.tas).to_dataset(name='vpd')
+    Y_Test = vpdTest.sel(time=slice('2024', None))
+
+    Y_test = create_predictdand_data(test_scenario)
+    vpd_truth = Y_test["vpd"]
+
+    return [get_nrmse_spatial(vpd_truth, preds['vpd']), get_nrmse_global(vpd_truth, preds['vpd']), get_nrmse(vpd_truth, preds['vpd'])]
+    
+
+def train_CNN(scenarios):
+    len_historical = 165
+    slider = 10
+
+    X_train = []
+    Y_train = []
+    data_path = 'train_val/'
+    
+    for i, simu in enumerate(scenarios):
+    
+        input_name = 'inputs_' + simu + '.nc'
+
+        
+        input_xr = xr.open_dataset(data_path + input_name)
+        hursTrain = xr.open_dataset(f'vpd_data/hurs_{simu}.nc')
+        tasTrain = xr.open_dataset(f'vpd_data/tas_{simu}.nc')
+        output_xr = get_vpd(hursTrain.hurs, tasTrain.tas).to_dataset(name='vpd')
+        
+        print(input_xr.dims, simu)
+        
+        X_train.append(input_xr)
+        Y_train.append(output_xr)
+
+    # Compute mean/std of each variable for the whole dataset
+    meanstd_inputs = {}
+    
+    for var in ['CO2', 'CH4', 'SO2', 'BC']:
+        array = np.concatenate([X_train[i][var].data for i in [0, 1, 2]] + 
+                               [X_train[i][var].sel(time=slice(len_historical, None)).data for i in range(1, 3)])
+        print((array.mean(), array.std()))
+        meanstd_inputs[var] = (array.mean(), array.std())
+
+    # normalize input data 
+    X_train_norm = [] 
+    for i, train_xr in enumerate(X_train): 
+        for var in ['CO2', 'CH4', 'SO2', 'BC']: 
+            var_dims = train_xr[var].dims
+            train_xr=train_xr.assign({var: (var_dims, normalize(train_xr[var].data, var, meanstd_inputs))}) 
+        X_train_norm.append(train_xr)
+    var_to_predict = 'vpd'
+    X_train_all = np.concatenate([input_for_training(X_train_norm[i], slider=slider, len_historical=len_historical) for i in range(len(scenarios))], axis = 0)
+    Y_train_all = np.concatenate([output_for_training(Y_train[i], var_to_predict, slider=slider, len_historical=len_historical) for i in range(len(scenarios))], axis=0)
+
+    keras.backend.clear_session()
+    cnn_model = None
+
+    cnn_model = Sequential()
+    cnn_model.add(Input(shape=(slider, 96, 144, 4)))
+    cnn_model.add(TimeDistributed(Conv2D(20, (3, 3), padding='same', activation='relu'), input_shape=(slider, 96, 144, 4)))
+    cnn_model.add(TimeDistributed(AveragePooling2D(2)))
+    cnn_model.add(TimeDistributed(GlobalAveragePooling2D()))
+    cnn_model.add(LSTM(25, activation='relu'))
+    cnn_model.add(Dense(1*96*144))
+    cnn_model.add(Activation('linear'))
+    cnn_model.add(Reshape((1, 96, 144)))
+
+    cnn_model.compile(optimizer='rmsprop', loss='mse', metrics=['mse'])
+
+    hist = cnn_model.fit(X_train_all,
+                     Y_train_all,
+                     use_multiprocessing=True, 
+                     #workers=5,
+                     batch_size=16,
+                     epochs=27,
+                     verbose=1)
+
+    X_test = xr.open_dataset('train_val/inputs_ssp245.nc')
+
+    for var in ['CO2', 'CH4', 'SO2', 'BC']:
+        var_dims = X_test[var].dims
+        X_test = X_test.assign({var: (var_dims, normalize(X_test[var].data, var, meanstd_inputs))}) 
+        
+    X_test_np = input_for_training(X_test, slider=slider, skip_historical=False, len_historical=len_historical)
+
+    # Make predictions using trained model 
+    predictions = cnn_model.predict(X_test_np)
+    # reshape to xarray 
+    predictions = predictions.reshape(predictions.shape[0], predictions.shape[2], predictions.shape[3])
+    predictions = xr.DataArray(predictions, dims=['time', 'lat', 'lon'], coords=[X_test.time.data[slider-1:], X_test.latitude.data, X_test.longitude.data])
+    predictions = predictions.transpose('lat', 'lon', 'time').sel(time=slice(2015, 2101)).to_dataset(name=var_to_predict)
+    
+    predictions.to_netcdf('ssp245_predict_vpd.nc'.format(var_to_predict), 'w')
+
+    return predictions
+    
+
+
+if __name__ == "__main__":
+    scenarios = [sys.argv[1], sys.argv[2], sys.argv[3]]
+    test_sim = 'ssp245'
+    predictions = train_CNN(scenarios)
+    rmses = get_rmse(test_sim, predictions)
+    print("Global: " + str(rmses[0]))
+    print(" Spatial: " + str(rmses[1]))
+    print(" Total: " + str(rmses[2]))
+
+    
+
---
+++ CNN_helper.py
@@ -0,0 +1,90 @@
+import numpy as np
+import xarray as xr
+import pandas as pd
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+import os
+from eofs.xarray import Eof
+
+import random
+
+len_historical = 165
+slider = 10
+
+def normalize(data, var, meanstd_dict):
+    mean = meanstd_dict[var][0]
+    std = meanstd_dict[var][1]
+    return (data - mean)/std
+
+def unnormalize(data, var, meanstd_dict):
+    mean = meanstd_dict[var][0]
+    std = meanstd_dict[var][1]
+    return data * std + mean
+
+def get_nrmse_spatial(truth, pred):
+    weights = np.cos(np.deg2rad(truth.lat))
+    truth = truth.sel(time=slice(2080,None))
+    pred = pred[-21:]
+    truth_total = np.abs(truth.weighted(weights).mean(['lat', 'lon']).data.mean())
+    rmse_spatial = np.sqrt(((truth - pred).mean('time')**2).weighted(weights).mean(['lat','lon'])).data
+    return rmse_spatial / truth_total 
+
+def get_nrmse_global(truth, pred):
+    weights = np.cos(np.deg2rad(truth.lat))
+    truth = truth.sel(time=slice(2080,None))
+    pred = pred[-21:]
+    truth_total = np.abs(truth.weighted(weights).mean(['lat', 'lon']).data.mean())
+    rmse_global = np.sqrt((((truth - pred).weighted(weights).mean(['lat', 'lon']))**2).data.mean())
+    return rmse_global / truth_total 
+
+def get_nrmse(truth, pred):
+    return get_nrmse_spatial(truth, pred) + 5 * get_nrmse_global(truth, pred)
+
+def get_vpd(humidity_data, tas_data):
+    svp = 0.6112 * np.exp(17.76*(tas_data-273)/((tas_data-273) + 243.5))
+    vpd = svp * (1 - humidity_data/100)
+    return vpd
+
+# Functions for reshaping the data 
+def input_for_training(X_train_xr, slider, skip_historical=False, len_historical=None): 
+    
+    X_train_np =  X_train_xr.to_array().transpose('time', 'latitude', 'longitude', 'variable').data
+
+    time_length = X_train_np.shape[0]
+    # If we skip historical data, the first sequence created has as last element the first scenario data point
+    if skip_historical:
+        X_train_to_return = np.array([X_train_np[i:i+slider] for i in range(len_historical-slider+1, time_length-slider+1)])
+    # Else we just go through the whole dataset historical + scenario (does not matter in the case of 'hist-GHG' and 'hist_aer')
+    else:
+        X_train_to_return = np.array([X_train_np[i:i+slider] for i in range(0, time_length-slider+1)])
+    
+    return X_train_to_return 
+
+
+def output_for_training(Y_train_xr, var, slider, skip_historical=False, len_historical=None): 
+    Y_train_np = Y_train_xr[var].data
+    
+    time_length = Y_train_np.shape[0]
+    
+    # If we skip historical data, the first sequence created has as target element the first scenario data point
+    if skip_historical:
+        Y_train_to_return = np.array([[Y_train_np[i+slider-1]] for i in range(len_historical-slider+1, time_length-slider+1)])
+    # Else we just go through the whole dataset historical + scenario (does not matter in the case of 'hist-GHG' and 'hist_aer')
+    else:
+        Y_train_to_return = np.array([[Y_train_np[i+slider-1]] for i in range(0, time_length-slider+1)])
+    
+    return Y_train_to_return
+
+def create_predictdand_data(data_sets):
+    data_path = 'vpd_data/'
+    if isinstance(data_sets, str):
+        data_sets = [data_sets]
+    tas_y = xr.concat([xr.open_dataset("vpd_data/tas_ssp245.nc") 
+               for file in data_sets], dim='time')
+    hurs_y = xr.concat([xr.open_dataset("vpd_data/hurs_ssp245.nc") 
+                   for file in data_sets], dim='time')
+    
+    Y = tas_y.merge(hurs_y)
+    vpd = get_vpd(hurs_y['hurs'],tas_y['tas'])
+    Y = Y.assign(vpd=vpd/1000)
+    return Y
